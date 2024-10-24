a60ee048285a41b52061325602d4920c63d92478
aldinh777 <aldi.nh789@gmail.com>
2024-10-25 00:56:55 +0800

    basic login logic, also that component is a mess

---
+++ app/server/views/page.tsx
@@ -0,0 +1,65 @@
+import { computed, state } from '@aldinh777/reactive';
+import { list } from '@aldinh777/reactive/list';
+import { login } from '../../../db/repositories/user.ts';
+import type { ServerContext } from '@aldinh777/kerad-core';
+
+export default async function Views(_: any, context: ServerContext) {
+    const status = state('not logged in');
+    const submitting = state(false);
+    const disabledStyle = computed(() => (submitting() ? 'display: none;' : 'display: block'));
+    const reverseDisabledStyle = computed(() => (!submitting() ? 'display: none;' : 'display: block'));
+    const submitButton = <button style={disabledStyle}>Login</button>;
+    const disabledButton = (
+        <button disabled="" style={reverseDisabledStyle}>
+            Login
+        </button>
+    );
+    const buttons = list([submitButton, disabledButton]);
+
+    context.onDismount(() => {
+        disabledStyle.stop();
+        reverseDisabledStyle.stop();
+    });
+
+    async function userLogin(form: FormData) {
+        submitting(true);
+        const username = (form.get('username') as string) || '';
+        const password = (form.get('password') as string) || '';
+        const user = await login(username, password);
+        if (user) {
+            status('success');
+        } else {
+            status('failed');
+        }
+        submitting(false);
+    }
+    return (
+        <div>
+            <h1>Login Test</h1>
+            <div>status : {status}</div>
+            <form on:submit={userLogin}>
+                <table>
+                    <tr>
+                        <th>
+                            <label for="username">Username</label>
+                        </th>
+                        <td>
+                            <input type="text" name="username" id="username" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            <label for="password">Password</label>
+                        </th>
+                        <td>
+                            <input type="text" name="password" id="password" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan={2}>{buttons}</td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    );
+}
