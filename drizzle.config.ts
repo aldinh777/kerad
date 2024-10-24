9280093b94f05283ef8a0c6a467d1e97644e5ede
aldinh777 <aldi.nh789@gmail.com>
2024-10-24 23:16:39 +0800

    remove db:push & db:seed script, still thinkin of a better way to do it

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
     schema: './db/schema.ts',
     dialect: 'mysql',
+    out: './db/migrations',
     dbCredentials: {
         url: process.env['DATABASE_URL']!
+    },
+    migrations: {
+        table: '_migrations'
     }
 });
