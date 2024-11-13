a8dd17c8b57438eb18b1f6671ad3a0f039a368f2
aldinh777 <aldi.nh789@gmail.com>
2024-11-13 22:59:10 +0800

    session stored login

--- app/server/auth/page.tsx
+++ app/server/auth/page.tsx
@@ -1,27 +1,35 @@
+import type { ServerContext } from '@aldinh777/kerad-core';
 import { computed, state } from '@aldinh777/reactive';
 import { login } from '../../../db/repositories/user.ts';
+import { sessionByCookie } from '../../../lib/tools/session.ts';
 
-export default function () {
+export default function (_: any, context: ServerContext) {
+    const sessionData = sessionByCookie(context);
     const error = state('');
     const success = state('');
+    const user = sessionData.get('user');
+
+    async function handleLogin(formData: FormData) {
+        const username = formData.get('username') as string;
+        const password = formData.get('password') as string;
+        const user = await login(username, password);
+        if (user) {
+            const roles = user.userRoles.map((userRole) => userRole.role?.name).join(' | ');
+            sessionData.set('user', user);
+            success('logged in successfully as ' + roles);
+            error('');
+        } else {
+            success('');
+            error('wrong username or password');
+        }
+    }
+
     return (
         <div>
+            <div>{user ? 'Logged In as : ' + user.username : 'Not Logged In'}</div>
+            <div>{user ? 'Roles : ' + user.userRoles.map((userRole: any) => userRole.role?.name).join(' | ') : ''}</div>
             <h3>Auth</h3>
-            <form
-                on:submit={async (formData: FormData) => {
-                    const username = formData.get('username') as string;
-                    const password = formData.get('password') as string;
-                    const user = await login(username, password);
-                    if (user) {
-                        const roles = user.userRoles.map((userRole) => userRole.role?.name).join(' | ');
-                        success('logged in successfully as ' + roles);
-                        error('');
-                    } else {
-                        success('');
-                        error('wrong username or password');
-                    }
-                }}
-            >
+            <form on:submit={handleLogin}>
                 {computed(() => (success() ? <div style={{ color: 'green' }}>{success}</div> : <></>))}
                 {computed(() => (error() ? <div style={{ color: 'red' }}>{error}</div> : <></>))}
                 <div>
@@ -34,6 +42,7 @@ export default function () {
                 </div>
                 <div>
                     <button type="submit">Login</button>
+                    <button on:click={() => sessionData.delete('user')}>Logout</button>
                 </div>
             </form>
         </div>
